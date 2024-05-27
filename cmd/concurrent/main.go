c755fb913d5dba642ce8d4dec2c7dd62acef4165
Andre Santos <andre@dexdev.com.br>
2024-05-26 21:14:03 -0300

    feat: go routines : for-select loop example 1

--- cmd/concurrent/main.go
+++ cmd/concurrent/main.go
@@ -5,24 +5,24 @@ import (
 	"log/slog"
 )
 
+// unbuffered channel is a sychronous communication -> make(chan string) -> no pass size
+// buffered channel is async communication -> make(chan string, 3)
 func main() {
-	myChannel := make(chan string)
-	anotherChannel := make(chan string)
+	charChannel := make(chan string, 3)
+	chars := []string{"a", "b", "c"}
 
-	go func() {
-		myChannel <- "data"
-	}()
-
-	go func() {
-		anotherChannel <- "cow"
-	}()
-
-	select {
-	case msgFromMyChannel := <-myChannel:
-		log(msgFromMyChannel)
-	case msgFromAnotherChannel := <-anotherChannel:
-		log(msgFromAnotherChannel)
+	for _, char := range chars {
+		select {
+		case charChannel <- char:
+		}
 	}
+
+	close(charChannel)
+
+	for result := range charChannel {
+		log(result)
+	}
+
 }
 
 func log(msg string) {
