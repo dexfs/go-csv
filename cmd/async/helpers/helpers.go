5dc2b338d54ada23667cb212d2c63287720e7d81
Andre Santos <andre@dexdev.com.br>
2024-05-27 23:48:22 -0300

    feat: helpers ChunkStringSlice

--- cmd/async/helpers/helpers.go
+++ cmd/async/helpers/helpers.go
@@ -50,3 +50,15 @@ func ReadAllFile(filename string) ([][]string, error) {
 
 	return reader.ReadAll()
 }
+
+func ChunkStringSlice(slice [][]string, chunkSize int) [][][]string {
+	var chunks [][][]string
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+		if end > len(slice) {
+			end = len(slice)
+		}
+		chunks = append(chunks, slice[i:end])
+	}
+	return chunks
+}
